ed7a52d91f23fc48fe891d6a08656f6fde84288c
Derrick Lee <derricklee43@gmail.com>
2022-02-16 12:20:02 -0700

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -7,17 +7,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p>echo<span>Study</span></p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Hello World 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
