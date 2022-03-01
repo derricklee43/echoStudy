044bc891562867dc9a500133a8b169b0457a92d7
jiejasonliu <jasonningliu@gmail.com>
2022-02-28 19:27:50 -0700

    Add sass, eslint, and prettier with auto-format to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>echo<span>Study</span></p>
         <p>
-          Hello World 
+          echo<span>Study</span>
         </p>
+        <p>Hello World</p>
       </header>
     </div>
   );
