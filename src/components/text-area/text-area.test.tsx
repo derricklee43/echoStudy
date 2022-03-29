97cae851eba4c253d234c9633d2324d616a44d82
jiejasonliu <jasonningliu@gmail.com>
2022-03-28 21:22:32 -0600

    Improve test formatting

--- src/components/text-area/text-area.test.tsx
+++ src/components/text-area/text-area.test.tsx
@@ -8,52 +8,49 @@ const TEST_PLACEHOLDER = 'TEST_PLACEHOLDER';
 const TEST_INITIAL_TEXT = 'TEST_INITIAL_TEXT';
 
 describe('TextArea', () => {
-  function getTextAreaElement(container: HTMLElement) {
-    return container.getElementsByClassName('c-text-area')[0];
-  }
-
   it('should render correctly with default props', () => {
     const { container } = render(<TextArea lines={LINES} />);
-    expect(container.getElementsByClassName('c-text-area')[0]).toHaveAttribute('rows', `${LINES}`);
+    const textArea = getTextArea(container);
+    expect(textArea).toHaveAttribute('rows', `${LINES}`);
   });
 
   it('should render label', () => {
     const { container } = render(<TextArea lines={LINES} label={TEST_LABEL} />);
-    expect(container.querySelector('.c-text-area-label')).toBeTruthy();
-
-    // cannot use getByLabelText since label does not have a 'for' attribute
-    const labelElement = screen.getByText(TEST_LABEL);
-    expect(labelElement).toBeInTheDocument();
+    const label = getTextAreaLabel(container);
+    expect(label).toBeTruthy();
+    expect(label).toBeInTheDocument();
   });
 
   it('should render placeholder', () => {
     const { container } = render(<TextArea lines={LINES} placeholder={TEST_PLACEHOLDER} />);
-    const placeholderElement = screen.getByPlaceholderText(TEST_PLACEHOLDER);
-    expect(placeholderElement).toBeInTheDocument();
+    const placeholder = screen.getByPlaceholderText(TEST_PLACEHOLDER);
+    expect(placeholder).toBeInTheDocument();
 
     // type 'some text'
-    const textAreaElement = getTextAreaElement(container);
-    fireEvent.change(textAreaElement, { target: { value: 'some text' } });
-    expect(textAreaElement).toHaveTextContent('some text');
-    expect(textAreaElement).not.toHaveTextContent(TEST_PLACEHOLDER);
+    const textArea = getTextArea(container);
+    fireEvent.change(textArea, { target: { value: 'some text' } });
+    expect(textArea).toHaveValue('some text');
+    expect(textArea).not.toHaveValue(TEST_PLACEHOLDER);
   });
 
   it('should work nicely with placeholder when initial text is set', () => {
     const { container } = render(
       <TextArea lines={LINES} placeholder={TEST_PLACEHOLDER} initialText={TEST_INITIAL_TEXT} />
     );
-    const textAreaElement = container.getElementsByClassName('c-text-area')[0];
-    expect(textAreaElement).toHaveTextContent(TEST_INITIAL_TEXT);
-    expect(textAreaElement).not.toHaveTextContent(TEST_PLACEHOLDER);
+    const textArea = getTextArea(container);
+    expect(textArea).toHaveValue(TEST_INITIAL_TEXT);
+    expect(textArea).not.toHaveValue(TEST_PLACEHOLDER);
   });
 
   it('should be immutable and show footer when readonly', () => {
     const { container } = render(
       <TextArea lines={LINES} initialText={TEST_INITIAL_TEXT} readonly={true} />
     );
-    const textAreaElement = container.getElementsByClassName('c-text-area')[0];
-    expect(textAreaElement).toHaveTextContent(TEST_INITIAL_TEXT);
-    expect(container.querySelector('.c-text-area-footer')).toBeTruthy();
+    const textArea = getTextArea(container);
+    expect(textArea).toHaveValue(TEST_INITIAL_TEXT);
+
+    const readonlyFooter = getTextAreaFooter(container);
+    expect(readonlyFooter).toBeTruthy();
   });
 
   it('should propagate state changes through onChange', () => {
@@ -63,11 +60,23 @@ describe('TextArea', () => {
     expect(callsRef.length).toBe(0);
 
     // type 'some text'
-    const textAreaElement = getTextAreaElement(container);
-    fireEvent.change(textAreaElement, { target: { value: 'some text' } });
+    const textArea = getTextArea(container);
+    fireEvent.change(textArea, { target: { value: 'some text' } });
     expect(callsRef.length).toBe(1);
 
     // check the first argument of the first call
     expect(callsRef[0][0]).toBe('some text');
   });
+
+  function getTextArea(container: HTMLElement): HTMLElement {
+    return container.getElementsByClassName('c-text-area')[0] as HTMLElement;
+  }
+
+  function getTextAreaLabel(container: HTMLElement): HTMLElement {
+    return container.getElementsByClassName('c-text-area-label')[0] as HTMLElement;
+  }
+
+  function getTextAreaFooter(container: HTMLElement): HTMLElement {
+    return container.getElementsByClassName('c-text-area-footer')[0] as HTMLElement;
+  }
 });
