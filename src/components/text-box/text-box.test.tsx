9b1c928e7fea8bc098fe86f940f7d5192c77f8df
jiejasonliu <jasonningliu@gmail.com>
2022-03-28 21:05:04 -0600

    Add tests for text box component

---
+++ src/components/text-box/text-box.test.tsx
@@ -0,0 +1,80 @@
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import { TextBox } from './text-box';
+
+const TEST_LABEL = 'TEST_LABEL';
+const TEST_PLACEHOLDER = 'TEST_PLACEHOLDER';
+const TEST_INITIAL_TEXT = 'TEST_INITIAL_TEXT';
+
+describe('TextBox', () => {
+  it('should render correctly with default props', () => {
+    const { container } = render(<TextBox />);
+    const input = getTextBoxInput(container);
+    expect(input).toHaveAttribute('type', 'text');
+  });
+
+  it('should render label', () => {
+    const { container } = render(<TextBox label={TEST_LABEL} />);
+    const label = getTextBoxLabel(container);
+    expect(label).toBeTruthy();
+    expect(label).toBeInTheDocument();
+  });
+
+  it('should render placeholder', () => {
+    const { container } = render(<TextBox placeholder={TEST_PLACEHOLDER} />);
+    const placeholder = screen.getByPlaceholderText(TEST_PLACEHOLDER);
+    expect(placeholder).toBeInTheDocument();
+
+    // type 'some text'
+    const input = getTextBoxInput(container);
+    fireEvent.change(input, { target: { value: 'some text' } });
+    expect(input).toHaveValue('some text');
+    expect(input).not.toHaveValue(TEST_PLACEHOLDER);
+  });
+
+  it('should work nicely with placeholder when initial text is set', () => {
+    const { container } = render(
+      <TextBox placeholder={TEST_PLACEHOLDER} initialText={TEST_INITIAL_TEXT} />
+    );
+    const input = getTextBoxInput(container);
+    expect(input).toHaveValue(TEST_INITIAL_TEXT);
+    expect(input).not.toHaveValue(TEST_PLACEHOLDER);
+  });
+
+  it('should change label to legend view when input is focused or non-empty', () => {
+    const { container } = render(<TextBox label={TEST_LABEL} />);
+    const input = getTextBoxInput(container);
+    const label = getTextBoxLabel(container);
+
+    const expectLabelIsLegend = () => expect(label).toHaveClass('as-legend');
+    const expectLabelIsNotLegend = () => expect(label).not.toHaveClass('as-legend');
+
+    // check when just focused then unfocus
+    expectLabelIsNotLegend();
+    input.focus();
+    expectLabelIsLegend();
+    input.blur();
+    expectLabelIsNotLegend();
+
+    // check when just non-empty then make empty again
+    expectLabelIsNotLegend();
+    fireEvent.change(input, { target: { value: 'some text' } });
+    expectLabelIsLegend();
+    fireEvent.change(input, { target: { value: '' } }); // make empty
+    expectLabelIsNotLegend();
+
+    // check when both focused and non-empty
+    expectLabelIsNotLegend();
+    input.focus();
+    fireEvent.change(input, { target: { value: 'some text' } });
+    expectLabelIsLegend();
+  });
+
+  function getTextBoxInput(container: HTMLElement): HTMLElement {
+    return container.getElementsByClassName('c-text-box-input')[0] as HTMLElement;
+  }
+
+  function getTextBoxLabel(container: HTMLElement): HTMLElement {
+    return container.getElementsByClassName('c-text-box-label')[0] as HTMLElement;
+  }
+});
