5d33cfef93a85f7a8eff3a4d94ae89c7135f2dea
Jie "Jason" Liu <jasonningliu@gmail.com>
2022-04-07 22:01:15 -0600

    Text area dark variant and animate label as legend (#19)
    * Add dark variant and label as a legend to text area component
    
    * Remove unused imports

--- src/components/text-area/text-area.test.tsx
+++ src/components/text-area/text-area.test.tsx
@@ -8,13 +8,22 @@ const TEST_PLACEHOLDER = 'TEST_PLACEHOLDER';
 const TEST_VALUE = 'TEST_VALUE';
 
 // text box state is lifted, use this component to propagate changes
-const MockTextArea = ({ lines, value, label, placeholder, readonly, onChange }: TextAreaProps) => {
+const MockTextArea = ({
+  lines,
+  value,
+  label,
+  animateLabelAsLegend,
+  placeholder,
+  readonly,
+  onChange,
+}: TextAreaProps) => {
   const [val, setVal] = useState(value ?? '');
   return (
     <TextArea
       lines={lines}
       value={val}
       label={label}
+      animateLabelAsLegend={animateLabelAsLegend}
       placeholder={placeholder}
       readonly={readonly}
       onChange={(v: string) => {
@@ -82,6 +91,54 @@ describe('TextArea', () => {
 
     // check the first argument of the first call
     expect(callsRef[0][0]).toBe('some text');
+  });
+
+  it('should change label to legend view when input is focused or non-empty', () => {
+    const { container } = render(<MockTextArea lines={LINES} label={TEST_LABEL} />);
+    const textArea = getTextArea(container);
+    const label = getTextAreaLabel(container);
+
+    const expectLabelIsLegend = () => expect(label).toHaveClass('as-legend');
+    const expectLabelIsNotLegend = () => expect(label).not.toHaveClass('as-legend');
+
+    // check when just focused then unfocus
+    expectLabelIsNotLegend();
+    textArea.focus();
+    expectLabelIsLegend();
+    textArea.blur();
+    expectLabelIsNotLegend();
+
+    // check when just non-empty then make empty again
+    expectLabelIsNotLegend();
+    fireEvent.change(textArea, { target: { value: 'some text' } });
+    expectLabelIsLegend();
+    fireEvent.change(textArea, { target: { value: '' } }); // make empty
+    expectLabelIsNotLegend();
+
+    // check when both focused and non-empty
+    expectLabelIsNotLegend();
+    textArea.focus();
+    fireEvent.change(textArea, { target: { value: 'some text' } });
+    expectLabelIsLegend();
+  });
+
+  it('should not animate label to legend if setting is disabled', () => {
+    const { container } = render(
+      <MockTextArea lines={LINES} label={TEST_LABEL} animateLabelAsLegend={false} />
+    );
+    const textArea = getTextArea(container);
+    const label = getTextAreaLabel(container);
+
+    const expectLabelIsNotLegend = () => expect(label).not.toHaveClass('as-legend');
+
+    // check when focused
+    expectLabelIsNotLegend();
+    textArea.focus();
+    expectLabelIsNotLegend();
+
+    // check with text (also still focused)
+    fireEvent.change(textArea, { target: { value: 'some text' } });
+    expectLabelIsNotLegend();
   });
 
   function getTextArea(container: HTMLElement): HTMLElement {
