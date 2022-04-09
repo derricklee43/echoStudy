40c5d0ffeb2bd0c6c44aaa62727426f148308e2f
Derrick Lee <derricklee43@gmail.com>
2022-04-08 18:12:37 -0600

    added icons and svgs

---
+++ src/assets/icons/arrow-icon/arrow-icon.tsx
@@ -0,0 +1,17 @@
+import './arrow.scss';
+import React from 'react';
+import { ReactComponent as DownArrow } from '../../svg/down-arrow.svg';
+
+interface ArrowProps {
+  variant?: 'dark' | 'light';
+  orientation?: 'up' | 'down';
+  className?: string;
+}
+
+export const ArrowIcon = ({
+  className = '',
+  variant = 'dark',
+  orientation = 'down',
+}: ArrowProps) => {
+  return <DownArrow className={`arrow ${variant} ${orientation} ${className}`} />;
+};
