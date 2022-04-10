30923d98b2b0c1744e4ff32269b812bc3d26923b
jiejasonliu <jasonningliu@gmail.com>
2022-04-09 19:13:39 -0600

    Fix failing test and disable 'esbuild-jest' for better test errors

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './app';
+import { BrowserRouter } from 'react-router-dom';
 
 test('renders learn react link', () => {
-  render(<App />);
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
   const linkElement = screen.getByPlaceholderText(/search my decks/i);
   expect(linkElement).toBeInTheDocument();
 });
