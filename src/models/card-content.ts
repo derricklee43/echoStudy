458be149d5709131ca1b2fcb384a8f9f6af77cfc
Derrick Lee <derricklee43@gmail.com>
2022-04-11 08:55:04 -0600

    exported CardLanguages

--- src/models/card-content.ts
+++ src/models/card-content.ts
@@ -1,8 +1,8 @@
 import { DeckLanguages } from './deck';
 
-const cardLanguages = ['Default', ...DeckLanguages] as const;
+export const CardLanguages = ['Default', ...DeckLanguages] as const;
 
-export type CardLanguage = typeof cardLanguages[number];
+export type CardLanguage = typeof CardLanguages[number];
 
 export interface CardContent {
   text: string;
