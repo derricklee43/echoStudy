ca4efebc5af303b5e896d69e1f70960430ededb8
jiejasonliu <jasonningliu@gmail.com>
2022-04-15 15:19:36 -0600

    Fix failing tests

--- src/app.test.tsx
+++ src/app.test.tsx
@@ -2,11 +2,14 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './app';
 import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
 
 test('renders learn react link', () => {
   render(
     <BrowserRouter>
-      <App />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
     </BrowserRouter>
   );
   const linkElement = screen.getByPlaceholderText(/search my decks/i);
