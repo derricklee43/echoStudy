408457987443214b3947b454ac6e4135355e6b78
jiejasonliu <jasonningliu@gmail.com>
2022-09-05 14:45:21 -0600

    feat: improve 404 not found page

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,6 +1,13 @@
 import React from 'react';
+import waveImage from '../../assets/images/wave.png';
 import './not-found-page.scss';
 
 export const NotFoundPage = () => {
-  return <div className="not-found-page">oopsie daisy! Page not found...</div>;
+  return (
+    <div className="not-found-page">
+      <div className="code">404.</div>
+      <div className="message">Oopsie daisy! Page not found...</div>
+      <img className="wave-image" src={waveImage} />
+    </div>
+  );
 };
