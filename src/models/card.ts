d23df7e80742305d51ee14b56874ecb2f9cc62e8
jiejasonliu <jasonningliu@gmail.com>
2022-09-13 11:58:52 -0600

    feat: update card client to deserialize dates

--- src/models/card.ts
+++ src/models/card.ts
@@ -7,8 +7,19 @@ export interface Card {
   front: CardContent;
   back: CardContent;
   score: number;
+  dateCreated: Date;
+  dateUpdated: Date;
+  dateTouched: Date;
 }
 
 export function createNewCard(): Card {
-  return { front: createNewCardContent(), back: createNewCardContent(), key: uuidv4(), score: 0 };
+  return {
+    front: createNewCardContent(),
+    back: createNewCardContent(),
+    key: uuidv4(),
+    score: 0,
+    dateCreated: new Date(),
+    dateUpdated: new Date(),
+    dateTouched: new Date(),
+  };
 }
