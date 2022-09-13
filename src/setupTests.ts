50f11278cc22027880ed936638fbefdb68bef6de
jiejasonliu <jasonningliu@gmail.com>
2022-09-13 11:13:19 -0600

    test: add jestrunner config and allow some log levels

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -10,8 +10,8 @@ import '@testing-library/jest-dom';
  */
 global.console = {
   ...console,
-  log: jest.fn(),
-  debug: jest.fn(),
+  // log: jest.fn(),
+  // debug: jest.fn(),
   info: jest.fn(),
   warn: jest.fn(),
   error: jest.fn(),
