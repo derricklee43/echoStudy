44cb552a34841989566be49c135a2a99abff5444
jiejasonliu <jasonningliu@gmail.com>
2022-09-22 15:15:44 -0600

    feat(hamburger-menu): remove orientation prop

--- src/assets/icons/hamburger-menu-icon/hamburger-menu-icon.tsx
+++ src/assets/icons/hamburger-menu-icon/hamburger-menu-icon.tsx
@@ -6,20 +6,15 @@ import './hamburger-menu-icon.scss';
 interface HamburgerMenuIconProps {
   className?: string;
   variant?: 'dark' | 'light';
-  orientation?: 'normal' | 'rotated';
   onClick?: (event: React.MouseEvent) => void;
 }
 
 export const HamburgerMenuIcon = ({
   className = '',
   variant = 'light',
-  orientation = 'normal',
-  onClick = noop,
+  onClick,
 }: HamburgerMenuIconProps) => {
   return (
-    <HamburgerMenuSvg
-      className={`hamburger-icon ${variant} ${orientation} ${className}`}
-      onClick={onClick}
-    />
+    <HamburgerMenuSvg className={`hamburger-icon ${variant} ${className}`} onClick={onClick} />
   );
 };
