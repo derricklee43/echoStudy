093e1429995c7994c1b19bd4c2a5555934934402
jiejasonliu <jasonningliu@gmail.com>
2022-09-23 19:50:11 -0600

    test: add tests for account popup components

---
+++ src/components/header/account-popup/account-popup.test.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import { fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { AccountPopup } from './account-popup';
+import { renderWithTestRoots } from '../../../app.test';
+import { noop } from '../../../helpers/func';
+
+describe('AccountPopup', () => {
+  const DEFAULT_CHILDREN = <div>test123</div>;
+
+  beforeEach(() => {
+    // setup portal element for modal to render on
+    document.body.innerHTML = `<div id="portal"></div>`;
+  });
+
+  it('should render correctly with default props', () => {
+    renderWithTestRoots(
+      <AccountPopup showTrigger={true} onClose={noop}>
+        {DEFAULT_CHILDREN}
+      </AccountPopup>
+    );
+    expectPopupIsVisible();
+  });
+
+  it('should hide when show is not triggered', () => {
+    renderWithTestRoots(
+      <AccountPopup showTrigger={false} onClose={noop}>
+        {DEFAULT_CHILDREN}
+      </AccountPopup>
+    );
+    expectPopupIsHidden();
+  });
+
+  it('should close itself on outside click', () => {
+    const mockOnClose = jest.fn();
+    renderWithTestRoots(
+      <AccountPopup showTrigger={true} onClose={mockOnClose}>
+        {DEFAULT_CHILDREN}
+      </AccountPopup>
+    );
+    expect(mockOnClose).not.toBeCalled();
+
+    // simulate clicking outside modal
+    userEvent.click(document.body);
+    expect(mockOnClose).toBeCalled();
+  });
+
+  it('should close itself on Escape pressed', () => {
+    const mockOnClose = jest.fn();
+    renderWithTestRoots(
+      <AccountPopup showTrigger={true} onClose={mockOnClose}>
+        {DEFAULT_CHILDREN}
+      </AccountPopup>
+    );
+    expect(mockOnClose).not.toBeCalled();
+
+    // simulate pressing Escape
+    fireEvent.keyDown(document, { key: 'Escape' });
+    expect(mockOnClose).toBeCalled();
+  });
+
+  function expectPopupIsHidden() {
+    const wrapper = document.querySelector('.c-account-popup-overlay');
+    expect(wrapper).toHaveClass('hidden');
+  }
+
+  function expectPopupIsVisible() {
+    const wrapper = document.querySelector('.c-account-popup-overlay');
+    expect(wrapper).toHaveClass('visible');
+  }
+});
---
+++ src/components/header/welcome-user/welcome-user.test.tsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import { prettyDOM, screen } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react-hooks';
+import userEvent from '@testing-library/user-event';
+import { enableFetchMocks } from 'jest-fetch-mock';
+import { MutableSnapshot } from 'recoil';
+import { WelcomeUser } from './welcome-user';
+import { renderWithTestRoots } from '../../../app.test';
+import { authJwtState, UserInfo, userInfoStateAsync } from '../../../state/auth-jwt';
+import { userDecksState } from '../../../state/user-decks';
+
+// provide fetchMock global and disable mocking initially
+enableFetchMocks();
+fetchMock.dontMock();
+
+describe('WelcomeUser', () => {
+  beforeEach(() => {
+    // setup portal element for modal to render on
+    document.body.innerHTML = `<div id="portal"></div>`;
+  });
+
+  afterEach(() => {
+    fetchMock.resetMocks();
+  });
+
+  it('should render correctly with default props', () => {
+    renderWithTestRoots(<WelcomeUser className="test-class-789" />);
+    expect(document.querySelectorAll('.test-class-789')).toHaveLength(1);
+  });
+
+  it('should render with fallback if user info state is empty', async () => {
+    renderWithTestRoots(<WelcomeUser />);
+    await flushMacrotasks();
+
+    // fallback is a 'log out' button
+    expect(screen.queryAllByText('hi, test1').length).toEqual(0);
+    expect(screen.queryAllByText('log out').length).toEqual(1);
+  });
+
+  it('should greet user if user info state is empty', async () => {
+    renderWithTestRoots(<WelcomeUser />, { recoilState: getInitRecoilState() });
+    await flushMacrotasks();
+
+    expect(screen.queryAllByText('hi, test1').length).not.toEqual(0);
+  });
+
+  it('should perform actions in account popup', async () => {
+    const mockButtonClick = jest.fn();
+    renderWithTestRoots(
+      <WelcomeUser onProfileClick={mockButtonClick} onLogoutClick={mockButtonClick} />,
+      { recoilState: getInitRecoilState() }
+    );
+    await flushMacrotasks();
+
+    // open account popup (not needed, but in case of any side effects)
+    const greetingElement = screen.queryAllByText('hi, test1')[0];
+    userEvent.click(greetingElement);
+    expect(mockButtonClick).toBeCalledTimes(0);
+
+    // click all buttons
+    const actionButtons = document.querySelectorAll('button');
+    actionButtons.forEach((button) => userEvent.click(button));
+    expect(mockButtonClick).toBeCalledTimes(2);
+  });
+
+  /**
+   * Provides a recoil state that setups the mocking of the `userInfoStateAsync` selector.
+   * Can optionally pass in the UserInfo to be mocked if desired.
+   */
+  function getInitRecoilState(userInfo?: UserInfo): (snapshot: MutableSnapshot) => void {
+    fetchMock.doMock();
+
+    // expected /users request from `userInfoState` selector --> to mocked response
+    fetchMock.mockResponse(
+      JSON.stringify(
+        userInfo ?? {
+          username: 'test1',
+          email: 'test1@test.com',
+        }
+      ),
+      { headers: { 'content-type': 'application/json' } }
+    );
+
+    // setting up this state causes the `userInfoState` selector to asynchronously update
+    return (snapshot: MutableSnapshot) => {
+      snapshot.set(authJwtState, { accessToken: 'test', refreshToken: 'test' });
+    };
+  }
+
+  async function flushMacrotasks() {
+    return act(
+      () =>
+        new Promise((resolve) => {
+          setTimeout(resolve, 100);
+          jest.runAllTimers();
+        })
+    );
+  }
+});
