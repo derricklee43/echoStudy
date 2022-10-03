d9cd6971614a5331b5d5e381c7a79de17a6ddb27
jiejasonliu <jasonningliu@gmail.com>
2022-10-03 01:51:40 -0600

    test: update card mock to use dateTouched

--- src/models/mock/card.mock.ts
+++ src/models/mock/card.mock.ts
@@ -8,29 +8,29 @@ import {
 } from './card-content.mock';
 import { Card, createNewCard } from '../card';
 
-export const getTestFoxCard = (score?: number, dateUpdated?: Date): Card => {
+export const getTestFoxCard = (score?: number, dateTouched?: Date): Card => {
   const testCard = createNewCard();
   testCard.front = getTestFoxFront();
   testCard.back = getTestFoxBack();
   testCard.score = score ?? 0;
-  testCard.dateUpdated = dateUpdated ?? testCard.dateUpdated;
+  testCard.dateTouched = dateTouched ?? testCard.dateTouched;
   return testCard;
 };
 
-export const getTestMouseCard = (score?: number, dateUpdated?: Date): Card => {
+export const getTestMouseCard = (score?: number, dateTouched?: Date): Card => {
   const testCard = createNewCard();
   testCard.front = getTestMouseFront();
   testCard.back = getTestMouseBack();
   testCard.score = score ?? 0;
-  testCard.dateUpdated = dateUpdated ?? testCard.dateUpdated;
+  testCard.dateTouched = dateTouched ?? testCard.dateTouched;
   return testCard;
 };
 
-export const getTestMonkeyCard = (score?: number, dateUpdated?: Date): Card => {
+export const getTestMonkeyCard = (score?: number, dateTouched?: Date): Card => {
   const testCard = createNewCard();
   testCard.front = getTestMonkeyFront();
   testCard.back = getTestMonkeyBack();
   testCard.score = score ?? 0;
-  testCard.dateUpdated = dateUpdated ?? testCard.dateUpdated;
+  testCard.dateTouched = dateTouched ?? testCard.dateTouched;
   return testCard;
 };
