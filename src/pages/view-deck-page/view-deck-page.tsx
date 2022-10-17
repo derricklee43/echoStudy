afcf0587286b0c8f334008862c9606e768c222c6
jiejasonliu <jasonningliu@gmail.com>
2022-10-17 11:03:17 -0600

    feat: improve spacing on mobile dimensions

--- src/pages/view-deck-page/view-deck-page.tsx
+++ src/pages/view-deck-page/view-deck-page.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Fade } from '@/animations/fade';
 import { Button } from '@/components/button/button';
@@ -7,7 +7,6 @@ import { ReadOnlyFlashcard } from '@/components/read-only-flashcard/read-only-fl
 import { getFormattedDate } from '@/helpers/time';
 import { useLazyAudioPlayer } from '@/hooks/use-lazy-audio-player';
 import { Deck } from '@/models/deck';
-import { StudyConfigPopup } from '@/pages/_shared/study-config-popup/study-config-popup';
 import { paths } from '@/routing/paths';
 import './view-deck-page.scss';
 
@@ -20,30 +19,28 @@ export const ViewDeckPage = ({ deck }: ViewDeckPageProps) => {
   const navigate = useNavigate();
 
   return (
-    <>
-      <Fade className="view-deck-page">
-        <div className="view-deck-header">
-          <PageHeader label={deck.metaData.title} />
-          <div>
-            <Button onClick={() => navigate(`${paths.study}/${deck.metaData.id}`)} size="medium">
-              study
-            </Button>
-            <Button onClick={() => navigate(`${paths.editDeck}/${deck.metaData.id}`)} size="medium">
-              edit
-            </Button>
-          </div>
+    <Fade className="view-deck-page">
+      <div className="view-deck-header">
+        <PageHeader label={deck.metaData.title} />
+        <div className="action-button-group">
+          <Button onClick={() => navigate(`${paths.study}/${deck.metaData.id}`)} size="medium">
+            study
+          </Button>
+          <Button onClick={() => navigate(`${paths.editDeck}/${deck.metaData.id}`)} size="medium">
+            edit
+          </Button>
         </div>
-        <p className="view-deck-deck-description">{deck.metaData.desc}</p>
-        <div>{`${deck.cards.length} cards`}</div>
-        <div>{`created ${getFormattedDate(deck.metaData.dateCreated)}`}</div>
-        <hr className="view-deck-divider" />
-        {deck.cards.length > 0 ? (
-          getFlashcards()
-        ) : (
-          <div className="empty-card-set-placeholder">you currently have no cards in this deck</div>
-        )}
-      </Fade>
-    </>
+      </div>
+      <p className="view-deck-deck-description">{deck.metaData.desc}</p>
+      <div>{`${deck.cards.length} cards`}</div>
+      <div>{`created ${getFormattedDate(deck.metaData.dateCreated)}`}</div>
+      <hr className="view-deck-divider" />
+      {deck.cards.length > 0 ? (
+        getFlashcards()
+      ) : (
+        <div className="empty-card-set-placeholder">you currently have no cards in this deck</div>
+      )}
+    </Fade>
   );
 
   function getFlashcards() {
