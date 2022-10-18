df252002a1c0e0e271a2e664bdec78108d4a74e8
Derrick <derricklee43@gmail.com>
2022-10-17 20:02:55 -0600

    fix profile-page

--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -5,10 +5,9 @@ import { BubbleDivider } from '@/components/bubble-divider/bubble-divider';
 import { DeckCover } from '@/components/deck-cover/deck-cover';
 import { LoadingPage } from '@/components/loading-page/loading-page';
 import { PageHeader } from '@/components/page-header/page-header';
-import { ProfilePicture } from '@/components/profile-picture/profile-picture';
+import { useAccountClient } from '@/hooks/api/use-account-client';
 import { useDecksClient } from '@/hooks/api/use-decks-client';
 import { Deck } from '@/models/deck';
-import { StudyConfigPopup } from '@/pages/_shared/study-config-popup/study-config-popup';
 import { paths } from '@/routing/paths';
 import { userInfoStateAsync } from '@/state/auth-jwt';
 import { userDecksSortedState, userDecksState } from '@/state/user-decks';
@@ -24,12 +23,14 @@ export const ProfilePage = () => {
 
 const AsyncProfilePage = () => {
   const navigate = useNavigate();
+  const accountClient = useAccountClient();
   const decksClient = useDecksClient();
 
   const setUserDecks = useSetRecoilState(userDecksState);
   const sortedDecks = useRecoilValue(userDecksSortedState);
   const userData = useRecoilValue(userInfoStateAsync);
 
+  const pfpUrl = accountClient.getProfilePictureUrl(userData?.email ?? '');
   const { privateDecks, publicDecks } = _reduceDecksByAccess();
 
   // fetch flashcard decks on load
@@ -43,11 +44,31 @@ const AsyncProfilePage = () => {
         <PageHeader label="my profile" />
       </div>
       <div className="user-details">
-        <ProfilePicture username={userData?.email} showGlow={true} />
+        <div className="profile-picture-container">
+          <img className="profile-picture" src={pfpUrl} loading="lazy" />
+        </div>
         <span className="username">{`@${userData?.username}`}</span>
         <span className="full-name">{userData?.email.toLowerCase()}</span>
         <span className="date-joined">member since 2022</span>
       </div>
+      <BubbleDivider
+        className="decks-divider"
+        variantType="drop-down-reveal"
+        label={`private decks (${privateDecks.length})`}
+      >
+        {privateDecks.length > 0 && (
+          <div className="decks-container">{getDeckCovers(privateDecks)}</div>
+        )}
+      </BubbleDivider>
+      <BubbleDivider
+        className="decks-divider"
+        variantType="drop-down-reveal"
+        label={`public decks (${publicDecks.length})`}
+      >
+        {publicDecks.length > 0 && (
+          <div className="decks-container">{getDeckCovers(publicDecks)}</div>
+        )}
+      </BubbleDivider>
     </div>
   );
 
